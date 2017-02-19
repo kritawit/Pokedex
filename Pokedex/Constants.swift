57c1129f45ee4524393378dfecdac6ce85d37f1e
Header-Develop <kritawitbunket@gmail.com>
2017-02-19 15:02:40 +0700

    Added Alamofire Library for used the pokeapi

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Header-Develop on 2/18/2560 BE.
+//  Copyright © 2560 Header-Devs. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 
 class Pokemon {
@@ -20,6 +21,65 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionTxt: String!
+    private var _pokemonURL: String!
+
+    var description: String {
+
+
+        if _description == nil {
+            _description = ""
+        }
+        return _descriptionç
+
+    }
+
+    var type: String {
+        if _type == nil {
+            _type = ""
+        }
+        return _type
+    }
+
+
+    var defense: String {
+        if _defense == nil {
+            _defense = ""
+        }
+        return _defense
+    }
+
+    var height: String {
+        if _height == nil {
+            _height = ""
+        }
+        return _height
+    }
+
+    var weight: String {
+        if _weight == nil {
+            _weight = ""
+        }
+
+        return _weight
+    }
+
+
+    var attack: String {
+        if _attack == nil {
+            _attack = ""
+        }
+        return _attack
+    }
+
+
+    var nextEvolutionTxt: String {
+        if _nextEvolutionTxt == nil {
+            _nextEvolutionTxt = ""
+        }
+        return _nextEvolutionTxt
+    }
+
+
 
     var name: String {
         return _name
@@ -28,14 +88,47 @@ class Pokemon {
     var pokedexId: Int {
         return _pokedexId
     }
-    
-    var description : String{
-        return _description
-    }
+
 
     init(name: String, pokedexId: Int) {
         self._name = name
         self._pokedexId = pokedexId
+
+        self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(self.pokedexId)/"
+
+
+    }
+
+    func downloadPokemonDetail(completed: @escaping DownloadComplete) {
+
+        Alamofire.request(URL(string: _pokemonURL)!).responseJSON { (response) in
+
+            if let dict = response.result.value as? Dictionary<String, AnyObject> {
+
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+
+                if let height = dict["height"] as? String {
+                    self._height = height
+                }
+
+                if let attack = dict["attack"] as? Int {
+                    self._attack = "\(attack)"
+                }
+
+                if let defense = dict["defense"] as? Int {
+                    self._defense = "\(defense)"
+                }
+
+            }
+            
+            completed()
+
+        }
+
+
+
     }
 
 }
